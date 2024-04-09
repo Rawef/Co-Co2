116fe93c0d9851f960e5e195d78f46e444ca97ed
leith101 <leithjendoubi@gmail.com>
2024-04-09 23:36:45 +0100

    leithh

--- Front/src/app/eventt/event.component.ts
+++ Front/src/app/eventt/event.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+interface EventData {
+  place: string;
+  time: string;
+  description: string;
+  imageUrl: string;
+}
 
 
 @Component({
@@ -14,6 +20,8 @@ export class EventComponent {
   time: string = '';
   description: string = '';
   imageUrl: string = '';
+  showCard: boolean = false;
+  events: EventData[] = [];
 
   constructor(private http: HttpClient , private snackBar: MatSnackBar) {}
 
@@ -27,13 +35,21 @@ export class EventComponent {
 
     this.http.post('http://localhost:8089/events/ajouterEvent', eventData)
       .subscribe(response => {
+        this.showCard = true;
         console.log('Event added successfully:', response);
         this.showNotification('Event added successfully!');
+        this.events.push(eventData);
+        this.place = '';
+        this.time = '';
+        this.description = '';
+        this.imageUrl = '';
         // You can perform additional actions upon successful addition
         // such as resetting the form or navigating to a different page.
       }, error => {
         console.error('Error adding event:', error);
         // Handle errors accordingly
+      
+    
       });
 
 
